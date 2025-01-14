e6bf0193c6ac866f2453d17e46c87cf9624c11cf
CharlotteDM <carolinemularczyk@gmail.com>
2025-01-14 17:58:07 +0100

    data check

--- shipping_code.R
+++ shipping_code.R
@@ -5,3 +5,11 @@ setwd(path)
 
 #Data Source: https://www.kaggle.com/datasets/prachi13/customer-analytics/data
 shipping_data <- read.csv("Train.csv", stringsAsFactors = FALSE)
+
+#data check
+head(shipping_data)
+tail(shipping_data)
+colnames(shipping_data)
+str(shipping_data)
+summary(shipping_data)
+dim(shipping_data)
